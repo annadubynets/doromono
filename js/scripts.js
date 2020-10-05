8357a24446ea8bb67c52583a178068fb15020791
Anna Dubynets <arogulina@gmail.com>
2020-10-05 22:00:54 +0300

    add header, main section, explore-catalog-section, shop-categories-section

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$('#searchForm').on('shown.bs.collapse', function () {
+    // focus input on collapse
+    $("#search").focus()
+})
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
